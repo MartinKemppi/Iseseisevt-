b4a585e078c6343d0420443977f7a200a7f3188e
opilane <opilane@HP-CZC2349HTR>
2023-04-24 11:41:06 +0300

    Add project files.

---
+++ Mein.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtöö
+{
+    internal class Mein
+    {
+        public static void Main(string[] args)
+        //4 var
+        {
+            while (true)
+            {
+                Console.WriteLine("Sisesta number ülesanne valimiseks:\n 1) - Kontrollib kas sõna on palindroom või mitte\n 2) - Ruutmatriks\n 3) - Sorteerimine sisend\n 4) - Välju");
+                string vastus = Console.ReadLine();
+
+                if (vastus == "1")
+                {
+                    OmaMoodul.Palindrom();
+                }
+                else if (vastus == "2")
+                {
+                    OmaMoodul.Kalk();
+                }
+                else if (vastus == "3")
+                {
+                    OmaMoodul.Sorteerimine();
+                }
+                else if (vastus == "4")
+                {
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine("Kirjuta ainult need numbrid, mis on ettevalmistatud");
+                }
+            }
+        }
+    }
+}
---
+++ OmaMoodul.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtöö
+{
+    internal class OmaMoodul
+    {
+        public static void Palindrom()
+        {
+            {
+                Console.WriteLine("Siseta tekst, et kontrollida kas see on palindroom");
+                string str = Console.ReadLine();
+                bool isPalindrome = IsPalindrome(str);
+                Console.WriteLine("'{0}' - {1} palindroom.", str, isPalindrome ? "" : "ei ole");
+            }
+
+            static bool IsPalindrome(string str)
+            {
+                str = str.Replace(" ", "").ToLower();
+
+                for (int i = 0; i < str.Length / 2; i++)
+                {
+                    if (str[i] != str[str.Length - i - 1])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            Console.WriteLine();
+        }
+
+        public static void Kalk()
+        {
+            {
+                int[,] matrix = 
+            {
+            {  1,  2,  3,  4 },
+            { -1, -2,  0,  -3 },
+            {  -4, -5, -6, 9 },
+            {  5,  6,  7,  8 }
+            };
+
+                int postulemus = 0;
+                int negtulemus = 0;
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (matrix[i, j] > 0)
+                        {
+                            postulemus++;
+                        }
+                        else if (matrix[i, j] < 0)
+                        {
+                            negtulemus++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("0 element on ({0}, {1})", i, j);
+                        }
+                    }
+                }
+                Console.WriteLine("Positiivse numbrite arv: {0}", postulemus);
+                Console.WriteLine("Negatiivse numbrite arv: {0}", negtulemus);
+            }
+            Console.WriteLine();
+        }
+
+        public static void Sorteerimine()
+        {
+            Console.WriteLine("Sisesta tekst:");
+            string input = Console.ReadLine();
+           
+            char[] sortedChars = input.ToCharArray();
+            Array.Sort(sortedChars);
+
+            string reversedSortedString = new string(sortedChars);
+            char[] reversedChars = reversedSortedString.ToCharArray();
+            Array.Reverse(reversedChars);
+            reversedSortedString = new string(reversedChars);
+
+            Console.WriteLine("Sisestatud tekst: {0}", input);
+            Console.WriteLine("Sorteeritud tekst: {0}", new string(sortedChars));
+            Console.WriteLine("Ümberpööratud sorteeritud tekst: {0}", reversedSortedString);
+            Console.WriteLine();
+        }
+    }
+}
